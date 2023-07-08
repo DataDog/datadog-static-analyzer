8bd34e91da283e3d6905281bd987d8e5f024b37b
Julien Delange <julien.delange@datadoghq.com>
2023-07-07 21:53:08 -0400

    synchronize rule execution

--- bins/src/bin/datadog-static-analyzer.rs
+++ bins/src/bin/datadog-static-analyzer.rs
@@ -211,7 +211,7 @@ fn main() -> Result<()> {
 
     // we always keep one thread free and some room for the management threads that monitor
     // the rule execution.
-    let num_threads = ((num_cpus::get() as f32 - 1.0) * 0.80) as usize;
+    let num_threads = ((num_cpus::get() as f32 - 1.0) * 0.75) as usize;
     rayon::ThreadPoolBuilder::new()
         .num_threads(num_threads)
         .build_global()?;
