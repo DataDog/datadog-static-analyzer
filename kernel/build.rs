57c1cf59b86f717924f03c9005eb65a86c6b36f7
Julien Delange <julien.delange@datadoghq.com>
2023-12-20 09:53:54 -0500

    downgrade dependencies

--- kernel/build.rs
+++ kernel/build.rs
@@ -21,10 +21,10 @@ fn create_deno_snapshot() {
             cargo_manifest_dir: env!("CARGO_MANIFEST_DIR"),
             snapshot_path,
             startup_snapshot: None,
-            skip_op_registration: false,
             extensions: vec![deno_extension::init_ops_and_esm()],
             compression_cb: None,
             with_runtime_cb: None,
+            snapshot_module_load_cb: None,
         },
     );
 }
