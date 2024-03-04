7f24cc04c1ea1d6f67e9d7e53ad8226d89e8dbd9
Jason Foral <jason.foral@datadoghq.com>
2024-03-04 17:47:12 -0500

    Update rule matcher

--- crates/secrets-core/src/rule.rs
+++ crates/secrets-core/src/rule.rs
@@ -3,67 +3,36 @@
 // Copyright 2024 Datadog, Inc.
 
 use crate::common::ByteSpan;
-use crate::location::PointSpan;
-use crate::matcher::{MatcherId, PatternId};
+use crate::location::{PointLocator, PointSpan};
+use crate::matcher::PatternId;
 use crate::validator::ValidatorId;
-use std::fmt::{Debug, Display, Formatter};
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::rc::Rc;
+use std::string::FromUtf8Error;
 use std::sync::Arc;
 
 /// A boolean logic expression supporting AND, OR, and NOT
 #[derive(Debug, Clone)]
-pub enum Predicate {
-    /// A boolean expression detailing whether the input data is considered a match.
-    Match {
-        input: VariableKind,
-        matcher_id: MatcherId,
+pub enum Expression {
+    /// A predicate stating that a pattern with the given `PatternId` must match the source.
+    IsMatch {
+        source: MatchSource,
         pattern_id: PatternId,
     },
-    // TODO: these will eventually be implemented.
-    And(Box<Predicate>, Box<Predicate>),
-    Or(Box<Predicate>, Box<Predicate>),
-    Not(Box<Predicate>),
+    /// Logical `AND`
+    And(Rc<Expression>, Rc<Expression>),
+    /// Logical `OR`
+    Or(Rc<Expression>, Rc<Expression>),
+    /// Logical `NOT`
+    Not(Rc<Expression>),
 }
 
-impl Predicate {}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum VariableKind {
-    /// A global variable that is contextual to the data. For example, if this is a file, this could
-    /// be the file path, or the file contents.
-    Global { variable: String, member: String },
-    /// A reference to the name of a capture from a [`PatternMatch`].
-    Capture { member: String },
-}
-
-impl VariableKind {
-    /// Creates a [`VariableKind::Global`]
-    pub fn global(variable: impl Into<String>, member: impl Into<String>) -> VariableKind {
-        VariableKind::Global {
-            variable: variable.into(),
-            member: member.into(),
-        }
-    }
-
-    /// Creats a [`VariableKind::Capture`]
-    pub fn capture(member: impl Into<String>) -> VariableKind {
-        VariableKind::Capture {
-            member: member.into(),
-        }
-    }
-}
-
-impl Display for VariableKind {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            VariableKind::Global { variable, member } => {
-                write!(f, "{}.{}", variable, member)
-            }
-            VariableKind::Capture { member } => {
-                write!(f, "CAPTURES.{}", member)
-            }
-        }
-    }
+/// A data source to search when evaluating a [`Rule`]'s matchers.
+#[derive(Debug, Clone)]
+pub enum MatchSource {
+    Capture(String),
+    Prior,
 }
 
 /// A unique id that identifies a [`Rule`].
@@ -85,120 +54,96 @@ impl<T: AsRef<str>> From<T> for RuleId {
     }
 }
 
-// TODO: no pub
+#[derive(Debug, Clone)]
 pub struct Rule {
-    pub id: RuleId,
-    pub match_logic: Vec<Rc<Predicate>>,
-    pub validators: Vec<ValidatorId>,
+    id: RuleId,
+    conditions: Vec<Rc<RuleCondition>>,
+    match_stages: Vec<Rc<Expression>>,
+    validators: Vec<ValidatorId>,
 }
 
 impl Rule {
     pub fn new(
         id: RuleId,
-        logic: impl Into<Vec<Predicate>>,
+        conditions: impl Into<Vec<RuleCondition>>,
+        match_stages: impl Into<Vec<Expression>>,
         validators: impl Into<Vec<ValidatorId>>,
     ) -> Self {
-        let logic = logic.into().into_iter().map(Rc::new).collect::<Vec<_>>();
+        let match_stages = match_stages.into().into_iter().map(Rc::new).collect();
+        let conditions = conditions.into().into_iter().map(Rc::new).collect();
         Self {
             id,
-            match_logic: logic,
+            conditions,
+            match_stages,
             validators: validators.into(),
         }
     }
-    pub fn match_logic(&self) -> &[Rc<Predicate>] {
-        &self.match_logic
+
+    pub fn id(&self) -> &RuleId {
+        &self.id
+    }
+
+    pub fn match_stages(&self) -> &[Rc<Expression>] {
+        &self.match_stages
     }
 }
 
-/// An owned variant of a [`PatternMatch`].
-///
-/// A more semantic difference, though, is that a [`RuleMatch`] represents a match that
-/// may have passed through multiple filters, whereas a [`PatternMatch`] only represents
-/// the results of a single filter.
+/// A predicate that must hold true for a rule to be considered "active" and able to generate a [`RuleMatch`].
+#[derive(Debug)]
+pub enum RuleCondition {
+    FilePath(GlobAssertion<String>),
+}
+
+/// A Unix-style glob with either a positive or negative assertion:
+/// * `MustMatch`: A pattern must match the provided glob
+/// * `MustNotMatch`: A pattern must not match the provided glob
+#[derive(Debug, Clone)]
+pub enum GlobAssertion<T> {
+    MustMatch(T),
+    MustNotMatch(T),
+}
+
+/// A string that has passed all of a rule's matcher stages.
 #[derive(Debug, Clone)]
 pub struct RuleMatch {
     /// The id of the [`Rule`] that triggered this match.
-    rule_id: RuleId,
+    pub rule_id: RuleId,
+    /// The top-level match
+    pub matched: LocatedString,
     /// The captures of this match that will be sent to a validator.
-    captures: LocatedCaptures,
+    pub captures: HashMap<String, LocatedString>,
 }
 
-impl RuleMatch {
-    /// Returns the id of the [`Rule`] that generated this `PatternMatch`.
-    pub fn rule_id(&self) -> &RuleId {
-        &self.rule_id
-    }
-
-    /// Returns a reference to this match's captures.
-    pub fn captures(&self) -> &LocatedCaptures {
-        &self.captures
-    }
-}
-
-/// An owned version of [`crate::capture::Captures`] that is additionally augmented with
-/// the surrounding context of the capture.
-#[derive(Debug, Clone)]
-pub struct LocatedCaptures {
-    surrounding_lines: LocatedString,
-    name_slots: Option<Arc<Vec<Option<String>>>>,
-    capture_slots: CaptureSlots,
-}
-
-impl LocatedCaptures {
-    /// Returns the lines surrounding the entire capture.
-    pub fn surrounding_lines(&self) -> &LocatedString {
-        &self.surrounding_lines
-    }
-
-    /// Returns the capture with the given name.
-    pub fn name(&self, name: impl AsRef<str>) -> Option<&LocatedString> {
-        // Iterate `name_slots` from 1..n, searching for a name match.
-        let name_slots = self.name_slots.as_ref()?;
-        let slot_index = name_slots.as_ref().iter().skip(1).position(|slot| {
-            slot.as_ref()
-                .is_some_and(|capture_name| name.as_ref() == capture_name.as_str())
-        })? + 1;
-
-        self.get(slot_index)
-    }
-
-    /// Returns the capture with the given index.
-    pub fn get(&self, index: usize) -> Option<&LocatedString> {
-        self.capture_slots.0.get(index).and_then(|ls| ls.as_ref())
-    }
-
-    /// Returns a [`Capture`] representing the entire match.
-    pub fn entire(&self) -> &LocatedString {
-        self.capture_slots
-            .0
-            .first()
-            .expect("capture slots should always have at least one member")
-            .as_ref()
-            .expect("the first capture should always be present")
-    }
-
-    pub fn captures_len(&self) -> usize {
-        self.capture_slots.0.len()
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-#[repr(transparent)]
-pub struct CaptureSlots(Vec<Option<LocatedString>>);
-
-/// A `String` that has been located within its parent. Provides metadata about:
+/// A `String` that has been located within its parent, providing metadata about the string's:
 /// * [`ByteSpan`]
 /// * [`PointSpan`]
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct LocatedString {
-    inner: String,
-    byte_span: ByteSpan,
-    point_span: PointSpan,
+    pub inner: String,
+    pub byte_span: ByteSpan,
+    pub point_span: PointSpan,
 }
 
 impl LocatedString {
     pub fn as_str(&self) -> &str {
         self.inner.as_str()
+    }
+
+    /// Creates a [`LocatedString`], given a `PointLocator`.
+    ///
+    /// # Panics
+    /// Panics if the `ByteSpan` would be out of bounds.
+    pub fn from_locator(
+        locator: &PointLocator,
+        byte_span: ByteSpan,
+    ) -> Result<LocatedString, FromUtf8Error> {
+        let point_span = locator.get_point_span(byte_span);
+        let inner = String::from_utf8(locator.data()[byte_span.as_range()].to_vec())?;
+        Ok(LocatedString {
+            inner,
+            byte_span,
+            point_span,
+        })
     }
 }
 
