42199d9da07567c98763e3d4e26d7e56dd451f09
Jason Foral <jason.foral@datadoghq.com>
2024-03-04 15:03:25 -0500

    Add (temporary) crate-wide clippy rules. This is done because certain parts of the library still have some scaffolding, so addressing the lints would be counterproductive.

--- crates/secrets-core/src/lib.rs
+++ crates/secrets-core/src/lib.rs
@@ -2,6 +2,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024 Datadog, Inc.
 
+// NOTE: Because this crate still has some scaffolding, these ignores are added
+// to (temporarily) silence clippy while iterating on the `pub` interface to expose.
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
 mod capture;
 mod common;
 mod location;
