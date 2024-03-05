ea9d5c0f6fae01ce2c61b9a7ad5db703d99829f9
Jacobo Tarrio <jacobo.tarriobarreiro@datadoghq.com>
2024-03-05 09:25:37 -0500

    When deserializing arguments, cast every value to a string.

--- crates/cli/src/model/serialization.rs
+++ crates/cli/src/model/serialization.rs
@@ -19,6 +19,35 @@ impl<'de> Deserialize<'de> for ArgumentValues {
                 formatter.write_str("a string or map from subtree to string")
             }
 
+            // Cast pretty much every primitive type to a string.
+            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E> where E: Error {
+                self.visit_string(v.to_string())
+            }
+
+            fn visit_u128<E>(self, v: u128) -> Result<Self::Value, E> where E: Error {
+                self.visit_string(v.to_string())
+            }
+
+            fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E> where E: Error {
+                self.visit_string(v.to_string())
+            }
+
+            fn visit_i128<E>(self, v: i128) -> Result<Self::Value, E> where E: Error {
+                self.visit_string(v.to_string())
+            }
+
+            fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E> where E: Error {
+                self.visit_string(v.to_string())
+            }
+
+            fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E> where E: Error {
+                self.visit_string(v.to_string())
+            }
+
+            fn visit_unit<E>(self) -> Result<Self::Value, E> where E: Error {
+                self.visit_string("".to_string())
+            }
+
             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
             where
                 E: Error,
