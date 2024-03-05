3f8cc793672556d9b3a466bf560acaa04aa31dde
Jacobo Tarrio <jacobo.tarriobarreiro@datadoghq.com>
2024-03-05 16:30:46 -0500

    In the `analyze` request, receive and use the configuration file.

--- crates/static-analysis-server/src/constants.rs
+++ crates/static-analysis-server/src/constants.rs
@@ -2,6 +2,10 @@
 pub const ERROR_DECODING_BASE64: &str = "error-decoding-base64";
 // when the code is not base64
 pub const ERROR_CODE_NOT_BASE64: &str = "code-not-base64";
+// when the configuration file is not valid base64
+pub const ERROR_CONFIGURATION_NOT_BASE64: &str = "configuration-not-base64";
+// when it was not possible to parse the configuration file
+pub const ERROR_COULD_NOT_PARSE_CONFIGURATION: &str = "could-not-parse-configuration";
 // rules and core language are different
 pub const ERROR_CODE_LANGUAGE_MISMATCH: &str = "language-mismatch";
 // no root node when trying to get the AST
