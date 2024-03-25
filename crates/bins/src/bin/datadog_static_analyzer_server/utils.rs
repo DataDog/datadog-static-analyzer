344492d0d0dd56f293da68b8b96e8fb16003f13f
Roberto Huertas <roberto.huertas@outlook.com>
2024-03-25 21:28:30 +0100

    small refactor

--- crates/bins/src/bin/datadog_static_analyzer_server/utils.rs
+++ crates/bins/src/bin/datadog_static_analyzer_server/utils.rs
@@ -7,3 +7,13 @@ pub fn get_current_timestamp_ms() -> u128 {
         .unwrap()
         .as_millis()
 }
+
+/// gets the kernel version (CARGO)
+pub fn get_version() -> String {
+    kernel::constants::CARGO_VERSION.to_string()
+}
+
+// gets the kernel revision (VERSION)
+pub fn get_revision() -> String {
+    kernel::constants::VERSION.to_string()
+}
