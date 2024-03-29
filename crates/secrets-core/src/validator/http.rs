6a2b8aa0e8982dbbb77a81c460168677776f1174
Jason Foral <jason.foral@datadoghq.com>
2024-03-29 10:05:30 -0400

    Remove `retry` dependency

--- crates/secrets-core/src/validator/http.rs
+++ crates/secrets-core/src/validator/http.rs
@@ -7,7 +7,6 @@ use crate::validator::{Candidate, SecretCategory, Validator, ValidatorError, Val
 use governor::clock::DefaultClock;
 use governor::state::{InMemoryState, NotKeyed};
 use governor::RateLimiter;
-use retry::OperationResult;
 use std::collections::HashSet;
 use std::fmt::{Debug, Display, Formatter};
 use std::ops::Add;
@@ -21,16 +20,17 @@ pub(crate) enum HttpValidatorError {
     /// The validation hit an unrecoverable error, and no further attempts will be made.
     #[error("unrecoverable validation failure")]
     RequestedAbort(Box<Result<ureq::Response, ureq::Error>>),
-    #[error("validator requested retry")]
-    RequestedRetry,
     #[error("invalid url: `{0}` ({1})")]
     InvalidUrl(String, url::ParseError),
     #[error("unsupported HTTP method `{0}`")]
     InvalidMethod(String),
-    #[error("retrying rule validation timed out. elapsed: {0:?}")]
-    RetryTimeout(Duration),
-    #[error("retrying rule validation will soon time out. elapsed: {elapsed:?}")]
-    RetryWillTimeout {
+    #[error("validation attempt exceeded the time limit")]
+    RetryTimeExceeded { attempted: usize, elapsed: Duration },
+    #[error("all validation retry attempts used")]
+    RetryAttemptsExceeded { attempted: usize, elapsed: Duration },
+    #[error("validation retry will exceed the overall time limit")]
+    RetryWillExceedTime {
+        attempted: usize,
         elapsed: Duration,
         next_delay: Duration,
     },
@@ -103,11 +103,17 @@ impl Validator for HttpValidator {
         let start_time = Instant::now();
 
         let retry_delays = (self.retry_timings_iter)();
-        retry::retry(retry_delays, || {
+        let mut iter = retry_delays.peekable();
+        let mut attempted = 0;
+
+        while let Some(retry_delay) = iter.next() {
+            // Certain branches can add to the required sleep time, so we track this as a mutable variable.
+            let mut to_sleep = retry_delay;
+
             loop {
                 let elapsed = start_time.elapsed();
                 if elapsed > self.max_attempt_duration {
-                    return OperationResult::Err(HttpValidatorError::RetryTimeout(elapsed));
+                    return Err(HttpValidatorError::RetryTimeExceeded { attempted, elapsed }.into());
                 }
                 match self.rate_limiter.check() {
                     Ok(_) => break,
@@ -115,27 +121,22 @@ impl Validator for HttpValidator {
                         let next_delay = try_again_at.wait_time_from(Instant::now());
                         let elapsed = start_time.elapsed();
                         if elapsed.add(next_delay) > self.max_attempt_duration {
-                            return OperationResult::Err(HttpValidatorError::RetryWillTimeout {
+                            return Err(HttpValidatorError::RetryWillExceedTime {
+                                attempted,
                                 elapsed,
                                 next_delay,
-                            });
+                            }
+                            .into());
                         }
-                        thread::sleep(next_delay)
+                        thread::sleep(next_delay);
                     }
                 }
             }
 
             let formatted_url = (self.request_generator.format_url)(&candidate);
 
-            let url = match Url::parse(&formatted_url) {
-                Ok(url) => url,
-                Err(parse_error) => {
-                    return OperationResult::Err(HttpValidatorError::InvalidUrl(
-                        formatted_url,
-                        parse_error,
-                    ));
-                }
-            };
+            let url = Url::parse(&formatted_url)
+                .map_err(|parse_err| HttpValidatorError::InvalidUrl(formatted_url, parse_err))?;
 
             let mut request = self
                 .request_generator
@@ -143,6 +144,7 @@ impl Validator for HttpValidator {
                 .request(self.request_generator.method.as_ref(), url.as_str());
             (self.request_generator.add_headers)(&candidate, &mut request);
 
+            attempted += 1;
             let response = match &self.request_generator.method {
                 HttpMethod::Get => request.call(),
                 HttpMethod::Post => {
@@ -160,39 +162,53 @@ impl Validator for HttpValidator {
 
             match next_action {
                 NextAction::Abort => {
-                    OperationResult::Err(HttpValidatorError::RequestedAbort(Box::new(response)))
+                    return Err(HttpValidatorError::RequestedAbort(Box::new(response)).into());
                 }
-                NextAction::Retry => OperationResult::Retry(HttpValidatorError::RequestedRetry),
+                NextAction::Retry => {}
                 NextAction::RetryAfter(http_retry_after) => {
-                    // NOTE: It would be nice to subtract what we know will be our next retry delay from
-                    // the HTTP 429 Retry-After to maximize efficiency.
-                    // However, due to `retry` API constraints, we can't peek the next retry time delay, so
-                    // for now, we over-estimate the amount we need to sleep.
-                    let elapsed = start_time.elapsed();
-                    if elapsed.add(http_retry_after) > self.max_attempt_duration {
-                        OperationResult::Err(HttpValidatorError::RetryWillTimeout {
-                            elapsed,
-                            next_delay: http_retry_after,
-                        })
-                    } else {
-                        thread::sleep(http_retry_after);
-                        // The `retry` API is a bit awkward here -- because of the duration calculations we've just
-                        // done, the inner `HttpValidatorError::TimedOut` should never be returned because the
-                        // next run of the iterator should never cause a timeout. Thus, the `time_spent` we pass in
-                        // here isn't accurate, though it will not matter.
-                        OperationResult::Retry(HttpValidatorError::RetryTimeout(http_retry_after))
-                    }
+                    // Calculate the amount to sleep based on what we know our next sleep duration will be.
+                    // For example, if the `Retry-After` is 15 seconds, and our next sleep will be 10 seconds,
+                    // add an additional 5 seconds. If our next sleep is 20 seconds, add 0 seconds.
+                    to_sleep += http_retry_after
+                        .checked_sub(iter.peek().copied().unwrap_or_default())
+                        .unwrap_or_default();
                 }
-                NextAction::ReturnResult(result) => OperationResult::Ok(result),
+                NextAction::ReturnResult(result) => return Ok(result),
                 NextAction::Unhandled => {
-                    OperationResult::Err(HttpValidatorError::UnhandledResponse(Box::new(response)))
+                    return Err(HttpValidatorError::UnhandledResponse(Box::new(response)).into());
                 }
             }
-        })
-        .map_err(|err| ValidatorError::ChildError {
+
+            // Only sleep if this isn't the last attempt
+            if iter.peek().is_some() {
+                let elapsed = start_time.elapsed();
+                if (elapsed + to_sleep) >= self.max_attempt_duration {
+                    return Err(HttpValidatorError::RetryWillExceedTime {
+                        attempted,
+                        elapsed,
+                        next_delay: to_sleep,
+                    }
+                    .into());
+                }
+                thread::sleep(to_sleep);
+            }
+        }
+
+        // We're within our time budget but exhausted our retry budget
+        Err(HttpValidatorError::RetryAttemptsExceeded {
+            attempted,
+            elapsed: start_time.elapsed(),
+        }
+        .into())
+    }
+}
+
+impl From<HttpValidatorError> for ValidatorError {
+    fn from(value: HttpValidatorError) -> Self {
+        Self::ChildError {
             validator_type: "http".to_string(),
-            err: Box::new(err),
-        })
+            err: Box::new(value),
+        }
     }
 }
 
