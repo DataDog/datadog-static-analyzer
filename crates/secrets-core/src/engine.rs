fd426a78b9d1dae1eeb095be31efbdc2ce26ba23
Jason Foral <jason.foral@datadoghq.com>
2024-04-02 10:33:25 -0400

    Add support for validation to the secrets engine

--- crates/secrets-core/src/engine.rs
+++ crates/secrets-core/src/engine.rs
@@ -4,11 +4,12 @@
 
 use crate::common::ByteSpan;
 use crate::location::PointSpan;
-use crate::matcher::Matcher;
+use crate::matcher::{Matcher, MatcherId};
 use crate::rule::{Rule, RuleId};
-use crate::validator::{Candidate, SecretCategory, Validator};
+use crate::validator::{Candidate, SecretCategory, Validator, ValidatorError, ValidatorId};
 use crate::worker::{Worker, WorkerError};
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
@@ -16,6 +17,12 @@ use std::sync::{Arc, Mutex};
 
 #[derive(Debug, thiserror::Error)]
 pub enum EngineError {
+    #[error("unknown validator `{0}`")]
+    UnknownValidator(String),
+    #[error("unknown rule `{0}`")]
+    UnknownRule(String),
+    #[error(transparent)]
+    Validator(#[from] ValidatorError),
     #[error(transparent)]
     Io(#[from] io::Error),
     #[error(transparent)]
@@ -23,9 +30,11 @@ pub enum EngineError {
 }
 
 pub struct Engine {
-    rules: Vec<Rule>,
-    matchers: Vec<Matcher>,
-    validators: Vec<Arc<Mutex<Box<dyn Validator + Send + Sync>>>>,
+    rules: HashMap<RuleId, Rule>,
+    matchers: HashMap<MatcherId, Matcher>,
+    // Whereas `Rule` and `Matcher` are intended to be thread-local, a validator must be held
+    // in an Arc because it may need to respect a rate-limit across threads.
+    validators: HashMap<ValidatorId, Arc<dyn Validator + Send + Sync>>,
     run_validation: bool,
 }
 
@@ -47,8 +56,33 @@ impl Engine {
         })
     }
 
-    fn validate_candidate(&self, _candidate: Candidate) -> Result<ValidationResult, EngineError> {
-        todo!()
+    pub fn validate_candidate(
+        &self,
+        candidate: Candidate,
+    ) -> Result<ValidationResult, EngineError> {
+        let rule_id = &candidate.rule_match.rule_id;
+        let rule = self
+            .rules
+            .get(rule_id)
+            .ok_or_else(|| EngineError::UnknownRule(rule_id.as_ref().to_string()))?;
+        let validator = self.validators.get(rule.validator_id()).ok_or_else(|| {
+            EngineError::UnknownValidator(rule.validator_id().as_ref().to_string())
+        })?;
+
+        let source = candidate.source.clone();
+        let byte_span = candidate.rule_match.matched.byte_span;
+        let point_span = candidate.rule_match.matched.point_span;
+        let rule_id = rule_id.clone();
+        validator
+            .validate(candidate)
+            .map(|category| ValidationResult {
+                rule_id,
+                category,
+                source,
+                byte_span,
+                point_span,
+            })
+            .map_err(EngineError::Validator)
     }
 
     /// Creates a new Worker. This should be considered an expensive operation, as it will create a
@@ -56,10 +90,13 @@ impl Engine {
     fn init_worker(&self) -> Worker {
         let rules = self
             .rules
-            .iter()
+            .values()
             .map(|rule| Arc::new(rule.clone()))
             .collect::<Vec<_>>();
-        let matchers = self.matchers.clone();
+
+        // NOTE: This should be assumed to be expensive
+        let matchers = self.matchers.values().cloned().collect::<Vec<_>>();
+
         Worker::new(matchers, rules)
     }
 }
@@ -96,6 +133,14 @@ impl EngineBuilder {
         self
     }
 
+    pub fn validators(
+        mut self,
+        validators: impl IntoIterator<Item = Box<dyn Validator + Send + Sync>>,
+    ) -> Self {
+        self.validators.extend(validators);
+        self
+    }
+
     pub fn matcher(mut self, matcher: Matcher) -> Self {
         self.matchers.push(matcher);
         self
@@ -115,11 +160,21 @@ impl EngineBuilder {
         let validators = self
             .validators
             .into_iter()
-            .map(|validator| Arc::new(Mutex::new(validator)))
-            .collect::<Vec<_>>();
+            .map(|validator| (validator.id().clone(), Arc::from(validator)))
+            .collect::<HashMap<_, _>>();
+        let matchers = self
+            .matchers
+            .into_iter()
+            .map(|matcher| (matcher.id(), matcher))
+            .collect::<HashMap<_, _>>();
+        let rules = self
+            .rules
+            .into_iter()
+            .map(|rule| (rule.id().clone(), rule))
+            .collect::<HashMap<_, _>>();
         Engine {
-            rules: self.rules,
-            matchers: self.matchers,
+            rules,
+            matchers,
             validators,
             run_validation: self.run_validation,
         }
@@ -132,10 +187,34 @@ impl Default for EngineBuilder {
     }
 }
 
+/// A successful secret validation result, as well as its location within the source file.
+#[derive(Debug, Clone)]
 pub struct ValidationResult {
     rule_id: RuleId,
     category: SecretCategory,
     source: PathBuf,
     byte_span: ByteSpan,
     point_span: PointSpan,
+}
+
+impl ValidationResult {
+    pub fn rule_id(&self) -> &RuleId {
+        &self.rule_id
+    }
+
+    pub fn category(&self) -> SecretCategory {
+        self.category
+    }
+
+    pub fn source(&self) -> &Path {
+        self.source.as_path()
+    }
+
+    pub fn byte_span(&self) -> ByteSpan {
+        self.byte_span
+    }
+
+    pub fn point_span(&self) -> PointSpan {
+        self.point_span
+    }
 }
