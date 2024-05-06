6a597f1922e19afdc11cc23eaf2ed794b6f864a4
Julien Delange <julien.delange@datadoghq.com>
2024-05-06 16:23:38 -0400

    update

--- crates/static-analysis-kernel/src/analysis/tree_sitter.rs
+++ crates/static-analysis-kernel/src/analysis/tree_sitter.rs
@@ -89,7 +89,7 @@ pub fn get_query_nodes(
                 captures.insert(capture_name.to_string(), node.clone());
                 captures_list.entry(capture_name.to_string()).or_default();
                 captures_list
-                    .get_mut(&capture_name)
+                    .get_mut(&capture_name.to_string())
                     .unwrap()
                     .push(node.clone());
             }
