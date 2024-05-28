6c1339370d32df8b18531e6ecd2b49e916dea831
Jason Foral <jason.foral@datadoghq.com>
2024-05-28 09:13:55 -0400

    Add function to read a v8 function from a context.

--- crates/static-analysis-kernel/src/analysis/ddsa_lib/common.rs
+++ crates/static-analysis-kernel/src/analysis/ddsa_lib/common.rs
@@ -8,10 +8,41 @@ use std::fmt::Debug;
 
 #[derive(Debug, thiserror::Error)]
 pub enum DDSAJsRuntimeError {
+    #[error("expected `{name}` to exist within the v8 context")]
+    VariableNotFound { name: String },
+    #[error("type should be \"{expected}\", not \"{got}\"")]
+    WrongType {
+        expected: &'static str,
+        got: &'static str,
+    },
     /// A (shorthand) generic error used to fail a test. The test itself will have the appropriate context.
     #[cfg(test)]
     #[error("cfg(test): unspecified")]
     Unspecified,
+}
+
+/// Loads a global [`v8::Function`] from the provided scope, returning an error if either the
+/// identifier doesn't exist, or if it doesn't refer to a function.
+pub fn load_function(
+    scope: &mut HandleScope,
+    identifier: &str,
+) -> Result<v8::Global<v8::Function>, DDSAJsRuntimeError> {
+    let ctx = scope.get_current_context();
+    let global = ctx.global(scope);
+
+    let s_func_name = v8_interned(scope, identifier);
+    let id_value = global.get(scope, s_func_name.into()).ok_or_else(|| {
+        DDSAJsRuntimeError::VariableNotFound {
+            name: identifier.to_string(),
+        }
+    })?;
+    let func = v8::Local::<v8::Function>::try_from(id_value).map_err(|_| {
+        DDSAJsRuntimeError::WrongType {
+            expected: "function",
+            got: id_value.type_repr(),
+        }
+    })?;
+    Ok(v8::Global::new(scope, func))
 }
 
 /// Creates a [`Internalized`](v8::string::NewStringType::Internalized) v8 string. There is
