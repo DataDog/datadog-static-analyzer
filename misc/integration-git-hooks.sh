fcb0d1f0d8bfc0213f89b93af7cf97022db7c987
Julien Delange <julien.delange@datadoghq.com>
2024-07-20 17:40:08 -0400

    fix binary name

--- misc/integration-git-hooks.sh
+++ misc/integration-git-hooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo build --profile release-dev --bin datadog-static-analyzer-git-hooks
+cargo build --profile release-dev --bin datadog-static-analyzer-git-hook
 
 ## A Python repository
 REPO_DIR=$(mktemp -d)
@@ -30,7 +30,7 @@ SHA2=$(cd $REPO_DIR && git rev-parse HEAD)
 
 echo "Starting test: secrets should be found using the default branch"
 
-./target/release-dev/datadog-static-analyzer-git-hooks --repository "${REPO_DIR}" --secrets --debug yes --default-branch main >/tmp/plop 2>&1
+./target/release-dev/datadog-static-analyzer-git-hook --repository "${REPO_DIR}" --secrets --debug yes --default-branch main >/tmp/plop 2>&1
 
 if [ $? -ne 1 ]; then
   echo "secrets should have been found"
@@ -53,7 +53,7 @@ fi
 #############################################################
 echo "Starting test: secrets should be found using two sha"
 
-./target/release-dev/datadog-static-analyzer-git-hooks --repository "${REPO_DIR}" --secrets --debug yes --sha-start $SHA1 --sha-end $SHA2 >/tmp/plop 2>&1
+./target/release-dev/datadog-static-analyzer-git-hook --repository "${REPO_DIR}" --secrets --debug yes --sha-start $SHA1 --sha-end $SHA2 >/tmp/plop 2>&1
 
 if [ $? -ne 1 ]; then
   echo "secrets should have been found"
@@ -87,7 +87,7 @@ SHA3=$(cd $REPO_DIR && git rev-parse HEAD)
 
 echo "starting analyzer between $SHA2 and $SHA3"
 
-./target/release-dev/datadog-static-analyzer-git-hooks --repository "${REPO_DIR}" --secrets --debug yes --sha-start $SHA2 --sha-end $SHA3 >/tmp/plop 2>&1
+./target/release-dev/datadog-static-analyzer-git-hook --repository "${REPO_DIR}" --secrets --debug yes --sha-start $SHA2 --sha-end $SHA3 >/tmp/plop 2>&1
 
 if [ $? -ne 1 ]; then
   echo "static analysis issues should have been found"
@@ -111,7 +111,7 @@ fi
 
 echo "Starting test: error when a branch does not exists"
 
-./target/release-dev/datadog-static-analyzer-git-hooks --repository "${REPO_DIR}" --secrets --debug yes --default-branch mainwefwef >/tmp/plop 2>&1
+./target/release-dev/datadog-static-analyzer-git-hook --repository "${REPO_DIR}" --secrets --debug yes --default-branch mainwefwef >/tmp/plop 2>&1
 
 if [ $? -ne 1 ]; then
   echo "branch cannot be found"
