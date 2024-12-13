e7a16760e9e7667acea278461cbf525ea126a714
Jason Foral <jason.foral@datadoghq.com>
2024-12-13 07:50:24 -0500

    Rename file

--- misc/integration-test-classification.sh
+++ misc/integration-test-classification.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This tests the `datadog-static-analyzer` binary to ensure test file classification is output correctly.
-# See `./_test-classification.sh` for test logic.
+# See `./helpers/test-classification.sh` for test logic.
 
 REPO_DIR=$(mktemp -d)
 RESULTS_FILE="${REPO_DIR}/results.json"
@@ -11,7 +11,7 @@ cargo build --profile release-dev --bin datadog-static-analyzer
 ANALYSIS_CMD='cargo run --profile release-dev --bin datadog-static-analyzer -- --directory "${REPO_DIR}" -o "${RESULTS_FILE}" -f sarif'
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"${SCRIPT_DIR}/_test-classification.sh" "${ANALYSIS_CMD}" "${REPO_DIR}" "${RESULTS_FILE}" || {
+"${SCRIPT_DIR}/helpers/test-classification.sh" "${ANALYSIS_CMD}" "${REPO_DIR}" "${RESULTS_FILE}" || {
     rm -rf $(mktemp -d)
     echo "Test failed"
     exit 1
