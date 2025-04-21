DataDog/datadog-static-analyzer

commit b6152347c5c74a682b38414b04af930ff9ac4458

address some feedback

crates/bins/src/bin/datadog-static-analyzer-git-hook.rs
@@ -21,6 +21,7 @@ use datadog_static_analyzer::{secret_analysis, static_analysis};
 use getopts::Options;
 use git2::Repository;
 use itertools::Itertools;
+use kernel::analysis::ddsa_lib::v8_platform::initialize_v8;
 use kernel::classifiers::ArtifactClassification;
 use kernel::constants::{CARGO_VERSION, VERSION};
 use kernel::model::common::OutputFormat::Json;
@@ -481,7 +482,9 @@ fn main() -> Result<()> {
 
     if static_analysis_enabled {
         let languages = get_languages_for_rules(&configuration.rules);
+        let v8 = initialize_v8(configuration.get_num_threads() as u32);
         let execution_results_tentative = static_analysis(
+            v8,
             &configuration,
             &analysis_options,
             &files_to_analyze,
@@ -497,10 +500,7 @@ fn main() -> Result<()> {
             execution_results_tentative.expect("execution should have succeeded");
 
         let static_analysis_metadata = &execution_result.metadata;
-        all_rule_results = execution_result
-            .rule_results
-            .get_static_analysis_results()
-            .clone();
+        all_rule_results = execution_result.rule_results.clone();
 
         all_rule_results.iter_mut().for_each(|rr| {
             let path = PathBuf::from(&rr.filename);
@@ -557,7 +557,7 @@ fn main() -> Result<()> {
             }
         }
 
-        secrets_results = res.rule_results.get_secrets_results().clone();
+        secrets_results = res.rule_results;
 
         secrets_results.iter_mut().for_each(|rr| {
             let path = PathBuf::from(&rr.filename);

crates/bins/src/bin/datadog-static-analyzer.rs
@@ -32,6 +32,7 @@ use cli::violations_table;
 use common::analysis_options::AnalysisOptions;
 use common::model::diff_aware::DiffAware;
 use datadog_static_analyzer::{secret_analysis, static_analysis, CliResults};
+use kernel::analysis::ddsa_lib::v8_platform::initialize_v8;
 use kernel::analysis::generated_content::DEFAULT_IGNORED_GLOBS;
 use kernel::classifiers::ArtifactClassification;
 use kernel::constants::{CARGO_VERSION, VERSION};
@@ -44,7 +45,7 @@ use secrets::secret_files::should_ignore_file_for_secret;
 use std::collections::HashMap;
 use std::io::prelude::*;
 use std::process::exit;
-use std::time::{Duration, Instant, SystemTime};
+use std::time::{Duration, Instant};
 use std::{env, fs};
 
 fn print_usage(program: &str, opts: Options) {
@@ -511,10 +512,7 @@ fn main() -> Result<()> {
         );
     }
 
-    let global_start_timestamp = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
+    let global_start_time = Instant::now();
 
     let mut all_path_metadata = HashMap::<String, ArtifactClassification>::new();
     let mut result: CliResults = CliResults {
@@ -522,12 +520,11 @@ fn main() -> Result<()> {
         secrets: None,
     };
     if static_analysis_enabled {
-        let start_timestamp = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
+        let static_analysis_start = Instant::now();
+        let v8 = initialize_v8(configuration.get_num_threads() as u32);
 
         let execution_results_tentative = static_analysis(
+            v8,
             &configuration,
             &analysis_options,
             &files_to_analyze,
@@ -542,12 +539,7 @@ fn main() -> Result<()> {
         let execution_result =
             execution_results_tentative.expect("execution should have succeeded");
 
-        let end_timestamp = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-
-        let rules_results = execution_result.rule_results.get_static_analysis_results();
+        let rules_results = &execution_result.rule_results;
 
         let nb_violations: u32 = rules_results
             .iter()
@@ -566,13 +558,14 @@ fn main() -> Result<()> {
             .unique_by(|v| v.filename.as_str())
             .count();
 
-        let execution_time_secs = end_timestamp - start_timestamp;
-
         all_path_metadata.extend(static_analysis_metadata.clone());
 
         println!(
             "Found {} violation(s) in {} file(s) using {} rule(s) within {} sec(s)",
-            nb_violations, total_files_analyzed, number_of_rules_used, execution_time_secs
+            nb_violations,
+            total_files_analyzed,
+            number_of_rules_used,
+            static_analysis_start.elapsed().as_secs()
         );
 
         result.static_analysis = Some(execution_result);
@@ -598,7 +591,7 @@ fn main() -> Result<()> {
 
         let res = execution_results_tentative.expect("secrets should execute with success");
 
-        let secrets_rules_results = res.rule_results.get_secrets_results();
+        let secrets_rules_results = &res.rule_results;
 
         let nb_secrets_found: u32 = secrets_rules_results
             .iter()
@@ -622,8 +615,6 @@ fn main() -> Result<()> {
             }
         }
 
-        let secrets_execution_time_secs = secrets_start.elapsed().as_secs();
-
         let number_of_rules_used = secrets_rules_results
             .iter()
             .unique_by(|v| v.rule_name.as_str())
@@ -640,34 +631,28 @@ fn main() -> Result<()> {
             nb_secrets_validated,
             total_files_analyzed,
             number_of_rules_used,
-            secrets_execution_time_secs
+            secrets_start.elapsed().as_secs()
         );
 
         result.secrets = Some(res);
     }
 
-    let global_end_timestamp = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-
-    let global_execution_time = global_end_timestamp - global_start_timestamp;
+    let global_execution_time_secs = global_start_time.elapsed().as_secs();
 
     // if we have more than one static analysis violation and printing is enabled, show all
     // violations
     let static_analysis_rule_results = if let Some(sa) = &result.static_analysis {
-        sa.rule_results.get_static_analysis_results().clone()
+        sa.rule_results.clone()
     } else {
         vec![]
     };
     let secrets_violations = if let Some(secrets) = &result.secrets {
-        secrets.rule_results.get_secrets_results().clone()
+        secrets.rule_results.clone()
     } else {
         vec![]
     };
     let nb_total_static_analysis_violations: u32 = if let Some(sa) = &result.static_analysis {
         sa.rule_results
-            .get_static_analysis_results()
             .iter()
             .map(|x| x.violations.len() as u32)
             .sum()
@@ -710,7 +695,7 @@ fn main() -> Result<()> {
                 debug: configuration.use_debug,
                 config_digest: configuration.generate_diff_aware_digest(),
                 diff_aware_parameters,
-                execution_time_secs: global_execution_time,
+                execution_time_secs: global_execution_time_secs,
             },
             &all_path_metadata,
         )

crates/bins/src/lib.rs
@@ -1,4 +1,3 @@
-use crate::AnalysisRulesResult::{SecretsRulesResults, StaticAnalysisRulesResults};
 use cli::constants::EXIT_CODE_RULE_CHECKSUM_INVALID;
 use cli::file_utils::{filter_files_for_language, get_language_for_file};
 use cli::model::cli_configuration::CliConfiguration;
@@ -6,7 +5,7 @@ use cli::rule_utils::{check_rules_checksum, convert_rules_to_rules_internal};
 use common::analysis_options::AnalysisOptions;
 use indicatif::ProgressBar;
 use kernel::analysis::analyze::{analyze_with, generate_flow_graph_dot};
-use kernel::analysis::ddsa_lib::v8_platform::initialize_v8;
+use kernel::analysis::ddsa_lib::v8_platform::{Initialized, V8Platform};
 use kernel::analysis::ddsa_lib::JsRuntime;
 use kernel::classifiers::{is_test_file, ArtifactClassification};
 use kernel::model::analysis::ERROR_RULE_TIMEOUT;
@@ -38,53 +37,26 @@ pub fn read_file(path: &Path) -> anyhow::Result<String> {
 
 /// Structure to store all results from a CLI run.
 pub struct CliResults {
-    pub static_analysis: Option<AnalysisResult>,
-    pub secrets: Option<AnalysisResult>,
-}
-
-/// Represent the analysis rules results for either static analysis or secret. This is more
-/// of a helper object to store static analysis and secret rules.
-pub enum AnalysisRulesResult {
-    StaticAnalysisRulesResults(Vec<RuleResult>),
-    SecretsRulesResults(Vec<SecretResult>),
-}
-
-impl AnalysisRulesResult {
-    pub fn get_static_analysis_results(&self) -> &Vec<RuleResult> {
-        match self {
-            StaticAnalysisRulesResults(results) => results,
-            SecretsRulesResults(_) => {
-                panic!("trying to get secrets when only static analysis are stored")
-            }
-        }
-    }
-
-    pub fn get_secrets_results(&self) -> &Vec<SecretResult> {
-        match self {
-            StaticAnalysisRulesResults(_) => {
-                panic!("trying to get static analysis results when only secrets are stored")
-            }
-            SecretsRulesResults(results) => results,
-        }
-    }
+    pub static_analysis: Option<AnalysisResult<RuleResult>>,
+    pub secrets: Option<AnalysisResult<SecretResult>>,
 }
 
 /// Whatever is returned by static analysis or secrets, including debugging information
-pub struct AnalysisResult {
-    pub rule_results: AnalysisRulesResult,
+pub struct AnalysisResult<T> {
+    pub rule_results: Vec<T>,
     pub metadata: HashMap<String, ArtifactClassification>,
 }
 
 /// Run the static analysis pipeline. This is called for both the main binary and git hooks.
 pub fn static_analysis(
+    v8: V8Platform<Initialized>,
     config: &CliConfiguration,
     options: &AnalysisOptions,
     files_to_analyze: &[PathBuf],
     languages: &[Language],
-) -> anyhow::Result<AnalysisResult> {
+) -> anyhow::Result<AnalysisResult<RuleResult>> {
     let mut all_rule_results = Vec::<RuleResult>::new();
     let mut all_stats = AnalysisStatistics::new();
-    let v8 = initialize_v8(config.get_num_threads() as u32);
 
     let mut all_path_metadata_static_analysis =
         HashMap::<String, Option<ArtifactClassification>>::new();
@@ -281,7 +253,7 @@ pub fn static_analysis(
         .collect::<HashMap<_, _>>();
 
     let analyzer_result = AnalysisResult {
-        rule_results: StaticAnalysisRulesResults(all_rule_results),
+        rule_results: all_rule_results,
         metadata: all_path_metadata,
     };
     Ok(analyzer_result)
@@ -293,7 +265,7 @@ pub fn secret_analysis(
     config: &CliConfiguration,
     options: &AnalysisOptions,
     files_to_analyze: &[PathBuf],
-) -> anyhow::Result<AnalysisResult> {
+) -> anyhow::Result<AnalysisResult<SecretResult>> {
     let secrets_rules = &config.secrets_rules;
     let sds_scanner = build_sds_scanner(secrets_rules, config.use_debug);
 
@@ -375,7 +347,7 @@ pub fn secret_analysis(
     }
 
     let analyzer_result = AnalysisResult {
-        rule_results: SecretsRulesResults(secrets_results),
+        rule_results: secrets_results,
         metadata: path_metadata
             .into_iter()
             .filter_map(|(k, v)| v.map(|classification| (k, classification)))

commit 7659bf69fc1af0f7af9d17b8931a1b2fceff49cf

Fix regression in v8 initialization order introduced by b6152347c5c74a682b38414b04af930ff9ac4458

crates/bins/src/bin/datadog-static-analyzer-git-hook.rs
@@ -21,7 +21,7 @@ use datadog_static_analyzer::{secret_analysis, static_analysis};
 use getopts::Options;
 use git2::Repository;
 use itertools::Itertools;
-use kernel::analysis::ddsa_lib::v8_platform::initialize_v8;
+use kernel::analysis::ddsa_lib::v8_platform::{initialize_v8, Initialized, V8Platform};
 use kernel::classifiers::ArtifactClassification;
 use kernel::constants::{CARGO_VERSION, VERSION};
 use kernel::model::common::OutputFormat::Json;
@@ -392,6 +392,12 @@ fn main() -> Result<()> {
         }
     }
 
+    let mut v8: Option<V8Platform<Initialized>> = None;
+    if static_analysis_enabled {
+        let platform = initialize_v8(configuration.get_num_threads() as u32);
+        _ = v8.insert(platform)
+    }
+
     // This must be called _after_ `initialize_v8` (otherwise, PKU-related segfaults on Linux will occur).
     rayon::ThreadPoolBuilder::new()
         .num_threads(configuration.get_num_threads())
@@ -482,9 +488,8 @@ fn main() -> Result<()> {
 
     if static_analysis_enabled {
         let languages = get_languages_for_rules(&configuration.rules);
-        let v8 = initialize_v8(configuration.get_num_threads() as u32);
         let execution_result = static_analysis(
-            v8,
+            v8.expect("v8 should have been initialized manually"),
             &configuration,
             &analysis_options,
             &files_to_analyze,

crates/bins/src/bin/datadog-static-analyzer.rs
@@ -31,7 +31,7 @@ use cli::violations_table;
 use common::analysis_options::AnalysisOptions;
 use common::model::diff_aware::DiffAware;
 use datadog_static_analyzer::{secret_analysis, static_analysis, CliResults};
-use kernel::analysis::ddsa_lib::v8_platform::initialize_v8;
+use kernel::analysis::ddsa_lib::v8_platform::{initialize_v8, Initialized, V8Platform};
 use kernel::analysis::generated_content::DEFAULT_IGNORED_GLOBS;
 use kernel::classifiers::ArtifactClassification;
 use kernel::constants::{CARGO_VERSION, VERSION};
@@ -486,6 +486,12 @@ fn main() -> Result<()> {
         println!("diff aware data: {:?}", diff_aware_parameters);
     }
 
+    let mut v8: Option<V8Platform<Initialized>> = None;
+    if static_analysis_enabled {
+        let platform = initialize_v8(configuration.get_num_threads() as u32);
+        _ = v8.insert(platform)
+    }
+
     // This must be called _after_ `initialize_v8` (otherwise, PKU-related segfaults on Linux will occur).
     rayon::ThreadPoolBuilder::new()
         .num_threads(configuration.get_num_threads())
@@ -520,10 +526,9 @@ fn main() -> Result<()> {
     };
     if static_analysis_enabled {
         let static_analysis_start = Instant::now();
-        let v8 = initialize_v8(configuration.get_num_threads() as u32);
 
         let execution_result = static_analysis(
-            v8,
+            v8.expect("v8 should have been initialized manually"),
             &configuration,
             &analysis_options,
             &files_to_analyze,