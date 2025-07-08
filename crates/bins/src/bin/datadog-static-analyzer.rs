7cd912ec1a10ba4d40b7d9ba3e3512ecc6519b28
Joshua Delgado <joshua.delgado@datadoghq.com>
2025-07-08 16:12:48 -0400

    Formatting again

--- crates/bins/src/bin/datadog-static-analyzer.rs
+++ crates/bins/src/bin/datadog-static-analyzer.rs
@@ -196,10 +196,11 @@ fn main() -> Result<()> {
             vec![]
         }
     };
-    
+
     let output_file = matches
         .opt_str("o")
         .context("output file must be specified")?;
+    
     let output_format = match matches.opt_str("f") {
         Some(f) => match f.as_str() {
             "csv" => OutputFormat::Csv,
