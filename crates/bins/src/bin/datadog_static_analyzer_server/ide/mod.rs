c464a300a5bbc19dadc3a84071a5713a099e45a6
Roberto Huertas <roberto.huertas@outlook.com>
2025-09-03 20:22:28 +0200

    feat(ide): avoid GET url length issues (#756)
    ## What problem are you trying to solve?
    
    See https://github.com/DataDog/datadog-static-analyzer/pull/754 for more details.
    
    Essentially, the problem we may encounter is URL length being a problem for some of the GET endpoints.
    
    ## What is your solution?
    
    Added a v2 version for some GET endpoints that were expecting a base64 representation of a file content as URL path.
    
    We also added a new version for the POST rulesets endpoint just because the naming was more convenient.
    
    ## Alternatives considered
    
    ## What the reviewer should know
    
    This is a continuation of https://github.com/DataDog/datadog-static-analyzer/pull/754
    
    The idea is to remove the v1 endpoints in 6 months (or re-evaluate).
    
    IDE-4957

--- crates/bins/src/bin/datadog_static_analyzer_server/ide/mod.rs
+++ crates/bins/src/bin/datadog_static_analyzer_server/ide/mod.rs
@@ -5,9 +5,333 @@ use rocket::Route;
 
 pub fn ide_routes() -> Vec<Route> {
     rocket::routes![
-        configuration_file::endpoints::ignore_rule,
-        configuration_file::endpoints::post_rulesets,
-        configuration_file::endpoints::get_rulesets,
-        configuration_file::endpoints::can_onboard
+        configuration_file::endpoints::post_ignore_rule,
+        configuration_file::endpoints::get_can_onboard,
+        configuration_file::endpoints::post_can_onboard_v2,
+        configuration_file::endpoints::get_get_rulesets,
+        configuration_file::endpoints::post_get_rulesets_v2,
+        configuration_file::endpoints::post_add_rulesets,
+        configuration_file::endpoints::post_add_rulesets_v2,
     ]
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use kernel::utils::encode_base64_string;
+    use rocket::{
+        form::validate::Contains,
+        http::{ContentType, Status},
+        local::blocking::Client,
+        uri, Rocket,
+    };
+
+    use crate::datadog_static_analyzer_server::ide::ide_routes;
+
+    fn mount_rocket() -> Rocket<rocket::Build> {
+        rocket::build().mount("/", ide_routes())
+    }
+
+    const NORMAL_CONFIGURATION: &'static str = r#"schema-version: v1
+rulesets:
+- java-1
+- java-security"#;
+
+    const PARSE_ERROR_CONFIGURATION: &'static str = r#"schema-version: v50000
+rulesets:
+- java-1
+- java-security
+only:
+- ignore/domain"#;
+
+    const FALSY_CONFIGURATION: &'static str = r#"schema-version: v1
+rulesets:
+- java-1
+- java-security
+only:
+- ignore/domain"#;
+
+    const WITH_SLASH_CONFIGURATION: &'static str = r#"schema-version: v1
+rulesets:
+- java-1-??
+- java-security"#;
+
+    #[test]
+    fn post_ignore_rule() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(super::configuration_file::endpoints::post_ignore_rule))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "rule": "ruleset1/rule1",
+                "configuration": "{config}", 
+                "encoded": false 
+            }}"#
+            ))
+            .dispatch();
+
+        let expected = r#"schema-version: v1
+rulesets:
+  - java-1
+  - java-security
+  - ruleset1:
+    rules:
+      rule1:
+        ignore:
+          - "**"
+"#;
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), expected);
+    }
+
+    #[test]
+    fn get_can_onboard_v1() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let uri = uri!(super::configuration_file::endpoints::get_can_onboard(
+            PathBuf::from(config)
+        ));
+
+        let response = client.get(uri).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "true");
+    }
+
+    #[test]
+    fn get_can_onboard_v1_returns_parse_error() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(PARSE_ERROR_CONFIGURATION.to_string());
+
+        let uri = uri!(super::configuration_file::endpoints::get_can_onboard(
+            PathBuf::from(config)
+        ));
+
+        let response = client.get(uri).dispatch();
+
+        assert_eq!(response.status(), Status::InternalServerError);
+        assert!(response.into_string().contains("Error parsing yaml file"));
+    }
+
+    #[test]
+    fn get_can_onboard_v1_returns_false() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(FALSY_CONFIGURATION.to_string());
+
+        let uri = uri!(super::configuration_file::endpoints::get_can_onboard(
+            PathBuf::from(config)
+        ));
+
+        let response = client.get(uri).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "false");
+    }
+
+    #[test]
+    fn get_can_onboard_v1_does_not_fail_because_of_slash_in_content() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(WITH_SLASH_CONFIGURATION.to_string());
+
+        assert!(config.contains("/"), "{}", config);
+
+        let uri = uri!(super::configuration_file::endpoints::get_can_onboard(
+            PathBuf::from(config)
+        ));
+
+        let response = client.get(uri).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "true");
+    }
+
+    #[test]
+    fn get_can_onboard_v2() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(
+                super::configuration_file::endpoints::post_can_onboard_v2
+            ))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "configuration": "{config}"
+            }}"#
+            ))
+            .dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "true");
+    }
+
+    #[test]
+    fn get_can_onboard_v2_returns_parse_error() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(PARSE_ERROR_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(
+                super::configuration_file::endpoints::post_can_onboard_v2
+            ))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "configuration": "{config}"
+            }}"#
+            ))
+            .dispatch();
+
+        assert_eq!(response.status(), Status::InternalServerError);
+        assert!(response.into_string().contains("Error parsing yaml file"));
+    }
+
+    #[test]
+    fn get_can_onboard_v2_returns_false() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(FALSY_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(
+                super::configuration_file::endpoints::post_can_onboard_v2
+            ))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "configuration": "{config}"
+            }}"#
+            ))
+            .dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "false");
+    }
+
+    #[test]
+    fn get_rulesets_v1() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let uri = uri!(super::configuration_file::endpoints::get_get_rulesets(
+            PathBuf::from(config)
+        ));
+
+        let response = client.get(uri).dispatch();
+
+        let expected = r#"["java-1","java-security"]"#;
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), expected);
+    }
+
+    #[test]
+    fn get_rulesets_v1_does_not_fail_because_of_slash_in_content() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(WITH_SLASH_CONFIGURATION.to_string());
+
+        assert!(config.contains("/"));
+
+        let uri = uri!(super::configuration_file::endpoints::get_get_rulesets(
+            PathBuf::from(config)
+        ));
+
+        let response = client.get(uri).dispatch();
+
+        let expected = r#"["java-1-??","java-security"]"#;
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), expected);
+    }
+
+    #[test]
+    fn get_rulesets_v2() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(
+                super::configuration_file::endpoints::post_get_rulesets_v2
+            ))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "configuration": "{config}"
+            }}"#
+            ))
+            .dispatch();
+
+        let expected = r#"["java-1","java-security"]"#;
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), expected);
+    }
+
+    #[test]
+    fn add_rulesets_v1() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(
+                super::configuration_file::endpoints::post_add_rulesets
+            ))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "rulesets": ["ruleset1", "ruleset2"],
+                "configuration": "{config}",
+                "encoded": false
+            }}"#
+            ))
+            .dispatch();
+
+        let expected = r#"schema-version: v1
+rulesets:
+  - java-1
+  - java-security
+  - ruleset1
+  - ruleset2
+"#;
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), expected);
+    }
+
+    #[test]
+    fn add_rulesets_v2() {
+        let client = Client::tracked(mount_rocket()).expect("valid rocket instance");
+        let config = encode_base64_string(NORMAL_CONFIGURATION.to_string());
+
+        let response = client
+            .post(uri!(
+                super::configuration_file::endpoints::post_add_rulesets_v2
+            ))
+            .header(ContentType::JSON)
+            .body(format!(
+                r#"{{ 
+                "rulesets": ["ruleset1", "ruleset2"],
+                "configuration": "{config}",
+                "encoded": false
+            }}"#
+            ))
+            .dispatch();
+
+        let expected = r#"schema-version: v1
+rulesets:
+  - java-1
+  - java-security
+  - ruleset1
+  - ruleset2
+"#;
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), expected);
+    }
 }
