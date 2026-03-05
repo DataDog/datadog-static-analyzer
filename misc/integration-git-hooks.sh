dd74407ed7a86389697b482412cb31147efdc4dd
Jason Foral <jason.foral@datadoghq.com>
2026-03-05 10:05:04 -0500

    Migrate existing integration tests to use v1 config

--- misc/integration-git-hooks.sh
+++ misc/integration-git-hooks.sh
@@ -17,11 +17,15 @@ SHA1=$(cd $REPO_DIR && git rev-parse HEAD)
 
 ## Creating a new branch and adding a new file with a secret
 (cd "${REPO_DIR}" && git checkout -b new-branch)
-echo "rulesets:"> "${REPO_DIR}/static-analysis.datadog.yml"
-echo " - python-code-style" >> "${REPO_DIR}/static-analysis.datadog.yml"
+echo "schema-version: v1.0
+sast:
+  use-default-rulesets: false
+  use-rulesets:
+    - python-code-style
+" > "${REPO_DIR}/code-security.datadog.yaml"
 echo "aws_access_key_id AKIAIOSFODNN7EXAMPLE" > "${REPO_DIR}/foobar"
 (cd "${REPO_DIR}" && git add foobar)
-(cd "${REPO_DIR}" && git add static-analysis.datadog.yml)
+(cd "${REPO_DIR}" && git add code-security.datadog.yaml)
 (cd "${REPO_DIR}" && git commit -a -m"add foobar")
 SHA2=$(cd $REPO_DIR && git rev-parse HEAD)
 
